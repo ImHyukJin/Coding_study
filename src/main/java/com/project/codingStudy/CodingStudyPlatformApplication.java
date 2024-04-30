255ae69dad74a8dcfbebe15160811daa66d8e310
ImHyukJin <ohj9575@gmail.com>
2024-04-30 15:16:43 +0900

    2024/04/30

---
+++ src/main/java/com/project/codingStudy/CodingStudyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.project.codingStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingStudyPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingStudyPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/codingStudy/CodingStudyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.codingStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingStudyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
